5f002f807477fe33314a73a24b7a745a7e8a67ea
Edward Ponce <eponc006@ucr.edu>
2017-11-28 17:39:46 -0800

    Added Graphing Options phase1.m
    [+] added plot_type to select between plots

--- phase1.m
+++ phase1.m
@@ -1,9 +1,10 @@
-function [max_def] = phase1(a,b,h,E,v,q0,x0,y0,P,d,selection)
-%phase1(width,length,height,Elastic Modulus,Poisson Ratio,Distributed Load,Load X, Load Y,Point Load,diameter,selection)
+function [max_def] = phase1(a,b,h,E,v,q,x0,y0,P,d,selection,plot_type)
+%phase1(width,length,height,Elastic Modulus,Poisson Ratio,Distributed Load,Load X, Load Y,Point Load,diameter,selection,plot_type)
+%For plot_type: 1) contour 2) surf 3) 2D (only for circle)
 
 D=(E*h^3)/(12*(1-v^2));
 
-switch selection
+switch (selection)
     case 1 %square
         x = 0:1:a;
         y = 0:1:b;
@@ -11,9 +12,9 @@ switch selection
         Wc = zeros(size(X));
         Wd = zeros(size(X));
 
-        if q0 ~= 0
+        if q~= 0
             %deflection distributed rectangular
-            series_multiplier_distributed= (16*q0)/(pi^6*D);
+            series_multiplier_distributed= (16*q)/(pi^6*D);
             for m= 1:2:19
               for n = 1:2:19
 
@@ -21,11 +22,7 @@ switch selection
 
               end
             end
-            %contour(X,Y,Wd)
-            %surf(x,Y,Wd)
-            xlabel('x - pos (in)')
-            ylabel('y - pos (in)')
-            zlabel('Deflection (in)')
+
         end
 
         if P ~= 0
@@ -35,17 +32,28 @@ switch selection
                     Wc = Wc + ((4*P)/((pi^4)*a*b*D)).*(((sin((m.*pi.*x0)./a).*sin((n.*pi.*y0)./b))./(((m^2/a^2)+(n^2/b^2))^2)).*sin((m.*pi.*X)./a).*sin((n.*pi.*Y)./b));
                 end
             end
-            %surf(X,Y,Wc)
-            %contour(X,Y,Wc)
-            xlabel('x - pos (in)')
-            ylabel('in')
-            zlabel('in')
 
         end
 
           W = Wc + Wd;
           max_def=max(max(W));
-          contour(X,Y,W);
+          switch(plot_type)
+          case 1
+            contour(X,Y,W);
+            xlabel('x - pos ')
+            ylabel('y - pos ')
+            zlabel('Deflection ')
+            title({'Deflectoion of a Rectangular Plate';['Max Deflection = ',num2str(max_def)]})
+          case 2
+            surf(X,Y,W);
+            xlabel('x - pos ')
+            ylabel('y - pos ')
+            zlabel('Deflection ')
+            title({'Deflectoion of a Rectangular Plate';['Max Deflection = ',num2str(max_def)]})
+          otherwise
+            fprintf('Plot type selection does not exist\n')
+            fprintf('Using contour as default plot\n')
+            end
 
     case 2  %circular
 
@@ -57,9 +65,9 @@ switch selection
         Wc=zeros(size(r));
 
 
-        if q0 ~= 0
+        if q ~= 0
           %Distributed Deflection Circular Plate
-          Wd=((q0.*(a.^2-R.*2))./(64.*D)).*(((5+v)./(1+v)).*a.^2-R.^2);
+          Wd=((q.*(a.^2-R.*2))./(64.*D)).*(((5+v)./(1+v)).*a.^2-R.^2);
         end
         if P~=0
           %Point Deflection Circular Plate
@@ -70,5 +78,32 @@ switch selection
         max_def = max(max(W));
         X = R.*cos(THETA);
         Y = R.*sin(THETA);
-        contour(X,Y,W);
+
+        %Plot Selection
+        switch(plot_type)
+        case 1
+          contour(X,Y,W);
+          xlabel('x - pos ')
+          ylabel('y - pos ')
+          zlabel('Deflection ')
+          title({'Deflectoion of a Circular Plate';['Max Deflection = ',num2str(max_def)]})
+        case 2
+          surf(X,Y,W);
+          xlabel('x - pos ')
+          ylabel('y - pos ')
+          zlabel('Deflection ')
+          title({'Deflectoion of a Circular Plate';['Max Deflection = ',num2str(max_def)]})
+        case 3
+        plot(W,X);
+        xlabel('x - pos ')
+        ylabel('y - pos ')
+        zlabel('Deflection ')
+        title({'Deflectoion of a Circular Plate';['Max Deflection = ',num2str(max_def)]})
+
+        otherwise
+          fprintf('Plot type selection does not exist\n')
+          fprintf('Using surf as default plot\n')
+          surf(X,Y,W);
+        end
+
 end
