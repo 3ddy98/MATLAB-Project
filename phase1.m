ba90d69a0675570601f2ad3c3f87c748f66af099
Edward Ponce <eponc006@ucr.edu>
2017-11-28 21:51:00 -0800

    Final Phase File

--- phase1.m
+++ phase1.m
@@ -12,24 +12,22 @@ switch (selection)
         y = 0:b/50:b;
         % Creating a meshgrid to evaluate deflection on
         [X,Y] = meshgrid(x,y);
+        % Initialize vectors for point and distributed loads
         Wc = zeros(size(X));
         Wd = zeros(size(X));
 
-        if q~= 0
-            %deflection distributed rectangular
-            series_multiplier_distributed= (16*q)/(pi^6*D);
+        if q~= 0 % Distributed rectangular deflection
+
             for m= 1:2:19
               for n = 1:2:19
-
-                Wd = Wd + (series_multiplier_distributed).*(sin((m.*pi.*X)./a).*sin((n.*pi.*Y)./b))./((m.*n).*((m.^2./a.^2)+(n.^2./b.^2)).^2);
-
+                Wd = Wd + (16*q)/(pi^6*D).*(sin((m.*pi.*X)./a).*sin((n.*pi.*Y)./b))./((m.*n).*((m.^2./a.^2)+(n.^2./b.^2)).^2);
               end
             end
 
         end
 
-        if P ~= 0
-            %deflection point rectangular
+        if P ~= 0 % Distributed rectangular deflection
+
             for m = 1:1:10
                 for n = 1:1:10
                     Wc = Wc + ((4*P)/((pi^4)*a*b*D)).*(((sin((m.*pi.*x0)./a).*sin((n.*pi.*y0)./b))./(((m^2/a^2)+(n^2/b^2))^2)).*sin((m.*pi.*X)./a).*sin((n.*pi.*Y)./b));
@@ -38,16 +36,23 @@ switch (selection)
 
         end
 
+          % Summing both point load and distributed deflection
           W = Wc + Wd;
+          % Finding the maximum deflection on the plate
           max_def=max(max(W));
+          %switch to decide which plot to use
           switch(plot_type)
+          % Plotting Deflection (W)
           case 1
-            contour(X,Y,W);
+            [C,h] = contour(X,Y,W);
+            % Formatting and labeling figures
+            clabel(C,h)
             xlabel('x - pos ')
             ylabel('y - pos ')
             zlabel('Deflection ')
             title({'Deflection of a Rectangular Plate';['Max Deflection = ',num2str(max_def)]})
           case 2
+            % Formatting and labeling figures
             surf(X,Y,W);
             xlabel('x - pos ')
             ylabel('y - pos ')
@@ -56,58 +61,67 @@ switch (selection)
           otherwise
             fprintf('Plot type selection does not exist\n')
             fprintf('Using contour as default plot\n')
-            end
+           end
 
-    case 2  %circular
+    case 2  % Circular deflection
 
+        % Set range of values for variables
         a = d/2;
         r = 0:a/50:a;
         theta = 0:pi/24:2*pi;
+        % Creating a meshgrid to perform evaluations for deflection on
         [R, THETA] = meshgrid(r,theta);
+        % Initializing deflection vectors
         Wd=zeros(size(r));
         Wc=zeros(size(r));
 
 
-        if q ~= 0
+        if q ~= 0 % Distributed Deflection Circular Plate
           %Distributed Deflection Circular Plate
           Wd=((q.*(a.^2-R.*2))./(64.*D)).*(((5+v)./(1+v)).*a.^2-R.^2);
         end
-        if P~=0
+        if P~=0 % Point Deflection Circular Plate
           %Point Deflection Circular Plate
           Wc=(P)./(16.*pi.*D).*(((3+v)./(1+v)).*(a.^2-R.^2)+2.*R.^2.*log(R./a));
         end
-        %Total deflection of a point and distributed load on a circular plate
+        % Summing both point load and distributed deflection
         W = Wd + Wc;
-        %Max deflection of the total deflection
+        % Finding the maximum deflection on the plate
         max_def = max(max(W));
-        %convert to polar coordinates
+        % Converting R value to X and Y values to plot contour or surface
+        % plots
         X = R.*cos(THETA);
         Y = R.*sin(THETA);
 
+        % Plotting Deflection (W)
         %Plot Selection
         switch(plot_type)
         case 1
-          contour(X,Y,W);
+          [C,h] = contour(X,Y,W);
+          % Formatting and labeling figures
+          clabel(C,h)
           xlabel('x - pos ')
           ylabel('y - pos ')
           zlabel('Deflection ')
           title({'Deflection of a Circular Plate';['Max Deflection = ',num2str(max_def)]})
         case 2
           surf(X,Y,W);
+          % Formatting and labeling figures
           xlabel('x - pos ')
           ylabel('y - pos ')
           zlabel('Deflection ')
           title({'Deflection of a Circular Plate';['Max Deflection = ',num2str(max_def)]})
         case 3
         plot(r,W);
+        % Formatting and labeling figures
         xlabel('Radius ')
         ylabel('Deflection')
         title({'Deflection of a Circular Plate';['Max Deflection = ',num2str(max_def)]})
 
+        % Error output for plot type
         otherwise
           fprintf('Plot type selection does not exist\n')
           fprintf('Using surf as default plot\n')
           surf(X,Y,W);
         end
-
 end
